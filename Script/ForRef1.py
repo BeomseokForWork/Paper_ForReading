a9600700cda30e6cb17c108f471608e04287451a
BeomSeok <sessipss@gmail.com>
2022-08-09 11:01:01 -0400

    Ref1 folder

--- Script/ForRef1.py
+++ Script/ForRef1.py
@@ -1,5 +1,8 @@
 fileName = "G:\\My Drive\\_Documents\\_Read\\_Paper\\Crop Science\\2016\\56\\5\\P2731\\ref1.txt"
 f = open(fileName, "r")
 
+imsi = f.readline()
+print(imsi.split("//")[1] )
+
 print(f.readline())
-print(f.readline())
+print(f.read(100))
